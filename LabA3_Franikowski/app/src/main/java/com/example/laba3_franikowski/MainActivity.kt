dce161037b27418db9dc5bb7d0068f1c197587ff
Maciej Franikowski <mfranikowski@gmail.com>
2022-10-26 21:14:53 +0200

    Create timer application

--- LabA3_Franikowski/app/src/main/java/com/example/laba3_franikowski/MainActivity.kt
+++ LabA3_Franikowski/app/src/main/java/com/example/laba3_franikowski/MainActivity.kt
@@ -18,58 +18,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonPressed(view: View){
-        if(view.id == R.id.buttonClear) {
-            operation = ""
-            textViewResult.text = ""
-            return
-        }
-        if(isOperationButton(view) && (isOperationAlreadyPresent(textViewResult) || textViewResult.text.isEmpty()))
-            return
-
-        if(view.id == R.id.buttonCalculate){
-            calculate(textViewResult)
-            return
-        }
-
-
         val button: Button = view as Button
         textViewResult.text = textViewResult.text.toString().plus(button.text)
     }
 
-
-    private fun isOperationButton(view:View): Boolean {
-        val buttons:List<Int> = mutableListOf(
-            R.id.buttonPlus, R.id.buttonMinus,
-            R.id.buttonMultiply, R.id.buttonDivide
-        )
-        return buttons.contains(view.id)
-    }
-
-    private fun isOperationAlreadyPresent(textViewResult:TextView): Boolean {
-        return  textViewResult.text.toString().contains("+") ||
-                textViewResult.text.toString().contains("*") ||
-                textViewResult.text.toString().contains("/") ||
-                textViewResult.text.toString().contains("-")
-    }
-
-    private fun calculate(textViewResult:TextView){
-        val index : Int = textViewResult.text.indexOfAny(charArrayOf('+', '-', '*', '/'))
-        Log.i("index", index.toString())
-        val firstNumber: Int = textViewResult.text.subSequence(0, index).toString().toInt()
-        val secondNumber: Int = textViewResult.text.subSequence(index+1, textViewResult.text.length).toString().toInt()
-
-        Log.i("firstNum", firstNumber.toString())
-        Log.i("secondNum", secondNumber.toString())
-        Log.i("secondNum", textViewResult.text[index].toString())
-        when (textViewResult.text[index]) {
-            '+' -> textViewResult.text = (firstNumber + secondNumber).toString()
-            '-' -> textViewResult.text = (firstNumber - secondNumber).toString()
-            '*' -> textViewResult.text = (firstNumber * secondNumber).toString()
-            '/' -> textViewResult.text = (firstNumber / secondNumber).toString()
-            else -> {
-                System.out.printf("Error! operator is not correct")
-                return
-            }
-        }
-    }
 }
