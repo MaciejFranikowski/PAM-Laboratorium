51dca141631ee03e091cf2e6edf6b29c988d328f
Maciej Franikowski <mfranikowski@gmail.com>
2023-01-16 14:10:54 +0100

    Change navigator titles and probability calc

--- Lab2/Lab2/ResultViewController.swift
+++ Lab2/Lab2/ResultViewController.swift
@@ -14,23 +14,13 @@ class ResultViewController: UIViewController {
     var probability : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           
+        super.viewWillAppear(animated)
+        if(probability < 0){
+            probability = 0
+        }
         probabilityResultLabel.text = "The probability of having Covid-19 is calculated at \(probability)%."
        }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
