684840dbc88436ed9ddad6dee56ed80c48ec24c1
Maciej Franikowski <mfranikowski@gmail.com>
2023-01-17 15:32:05 +0100

    Add print in important ViewController funcs

--- Lab2/Lab2/ViewController.swift
+++ Lab2/Lab2/ViewController.swift
@@ -12,5 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidApper")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("viewWillLaytoutSubviews")
+    }
 }
 
